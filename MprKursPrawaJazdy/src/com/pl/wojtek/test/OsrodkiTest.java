bac46847b687d0aa14c9628f50c857f6d835d31a
student <student@student-vm>
2011-11-30 19:18:58 +0100

    Test JUnit.

---
+++ MprKursPrawaJazdy/src/com/pl/wojtek/test/OsrodkiTest.java
@@ -0,0 +1,165 @@
+package com.pl.wojtek.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import mprKursPrawaJazdy.Kursant;
+import mprKursPrawaJazdy.Kurs;
+import mprKursPrawaJazdy.Osrodki;
+
+public class OsrodkiTest {
+	
+	private List<Kursant> kursanci= new ArrayList<Kursant>();
+	private List<Kurs> kursy= new ArrayList<Kurs>();
+	private Osrodki Test = new Osrodki("Test", kursanci, kursy);
+	
+	@Before	
+	public void setUp() throws Exception {
+		
+		kursanci.add(new Kursant(1 , "nazwisko1"));
+		kursanci.add(new Kursant(2 , "nazwisko2"));
+		kursanci.add(new Kursant(3 , "nazwisko3"));
+		kursy.add(new Kurs (1, "nazwa1"));
+		kursy.add(new Kurs (2, "nazwa2"));
+		
+	}
+	
+	@After	
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void testOsrodkiStringListOfKursantListOfKurs() {
+		assertSame(Test.getKursanci(), kursanci);
+		assertEquals(Test.getKursy(), kursy);
+	}
+
+	@Test
+	public void testOsrodkiString() {
+		assertTrue(Test.getNazwa().equals("Test"));
+
+	}
+
+	@Test
+	public void testGetNazwa() {
+		assertTrue(Test.getNazwa().equals("Test"));
+	}
+
+	@Test
+	public void testSetNazwa() {
+		Test.setNazwa("Test1");
+		
+		assertTrue(Test.getNazwa().equals("Test1"));
+		assertSame("Test1", Test.getNazwa());
+	}
+
+	@Test
+	public void testGetKursy() {
+		assertTrue(Test.getKursy().equals(kursy));
+		assertFalse(Test.getKursy().equals(kursanci));
+	}
+
+	@Test
+	public void testAddKurs() {
+		assertTrue(kursy.size() == 2);
+		assertFalse(kursy.size() == 4);
+	}
+
+	@Test
+	public void testAddWieleKursow() {
+		assertTrue(kursy.size() == 2);
+		assertFalse(kursy.size() == 3);
+	}
+
+	@Test
+	public void testSetKursy() {
+		
+		assertSame(kursy, Test.getKursy());
+		assertEquals(Test.getKursy() , kursy);
+	}
+
+	@Test
+	public void testRemoveWieleKursow() {
+		Test.removeWieleKursow(Test.findKursPoNazwie("nazwisko3"));
+		
+		assertTrue(kursy.size() == 2);
+
+	}
+
+	@Test
+	public void testFindKursPoNazwie() {
+		assertNotNull(Test.findKursPoNazwie("nazwa1"));
+
+	}
+
+	@Test
+	public void testFindKursPoIDKursu() {
+		assertNotNull(Test.findKursPoIDKursu(1));
+	}
+
+	@Test
+	public void testSearchKurs() {
+		assertNotNull(Test.searchKurs(1, "nazwa1"));
+		//assertNull(Test.searchKurs(1,"name"));
+		//assertNull(("Brak kursu z ID:  1  o nazwie: nazwa1"),(Test.searchKurs(1, "nazwa1")));
+		
+	}
+
+	@Test
+	public void testEditWieleKursowPoID_kursuNazwie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetKursanci() {
+		assertTrue(Test.getKursanci().equals(kursanci));
+		assertFalse(Test.getKursanci().equals(kursy));	}
+
+	@Test
+	public void testAddKursant() {
+		assertTrue(kursanci.size() == 3);
+		assertFalse(kursanci.size() == 4);	}
+
+	@Test
+	public void testSetKursanci() {
+		assertSame(kursanci, Test.getKursanci());
+		assertEquals(Test.getKursanci(), kursanci);
+	}
+
+	@Test
+	public void testRemoveWieluKursantow() {
+		Test.removeWieluKursantow(Test.findKursantaPoNazwisku("nazwa2"));
+		
+		assertFalse(kursanci.size() == 2);
+		
+	}
+
+	@Test
+	public void testFindKursantaPoNazwisku() {
+		assertNotNull(Test.findKursantaPoNazwisku("nazwisko1"));	
+
+	}
+
+	@Test
+	public void testFindKursantaPoID() {
+		assertNotNull(Test.findKursantaPoID(2));
+	}
+
+	@Test
+	public void testSearchKursant() {
+		assertNotNull(Test.searchKursant(1, "nazwisko1"));	
+	}
+
+	@Test
+	public void testEditWieluKursantowPoID_numerNazwisko() {
+		fail("Not yet implemented");
+	}
+
+}
