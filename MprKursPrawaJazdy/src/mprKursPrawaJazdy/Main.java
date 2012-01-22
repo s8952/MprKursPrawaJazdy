0f6ecd2a2db24a13a6723095615c9c57ae0e38b1
student <student@student-vm>
2012-01-22 13:38:14 +0100

    DB READY

--- MprKursPrawaJazdy/src/mprKursPrawaJazdy/Main.java
+++ MprKursPrawaJazdy/src/mprKursPrawaJazdy/Main.java
@@ -3,6 +3,9 @@ package mprKursPrawaJazdy;
 import java.util.ArrayList;
 import java.util.List;
 
+import services.KursDBManager;
+import services.OsrodkiDBManager;
+
 	public class Main {
 
 			public static void main(String[] args) {
@@ -22,28 +25,28 @@ import java.util.List;
 			Gdansk.addKurs( 9 ,"KursCiezarowka");
 			Gdansk.addKurs( 11 ,"KursOsobowka");
 			Gdansk.addKurs(33, "KursHulajnoga");
-			
+
 			Gdansk.printAll();
-			
+
 			try	{
-			
+
 			Gdansk.removeWieluKursantow(Gdansk.findKursantaPoNazwisku("Baltazar Ciemny"));
 			Gdansk.removeWieluKursantow(Gdansk.findKursantaPoID(99));
 
 			Gdansk.removeWieleKursow(Gdansk.findKursPoIDKursu(11));
 			Gdansk.removeWieleKursow(Gdansk.findKursPoNazwie("KursHulajnoga"));
-			
+
 			Gdansk.printKursanci();	
 			Gdansk.printKursy();
-			
+
 			Gdansk.editWieluKursantowPoID_numerNazwisko(Gdansk.searchKursant(3,"Agnieszka Kowalska"),3, "Bruce Lee");
 			Gdansk.editWieleKursowPoID_kursuNazwie(Gdansk.searchKurs(1,"KursSztaplarka"),4, "KursOdzutowiec");
-			
+
 			Gdansk.printKursanci();	
 			Gdansk.printKursy();
-			
+
 			Gdansk.editWieleKursowPoID_kursuNazwie(Gdansk.searchKurs(9,"KursCiezarowka"),2, "KursCiezarowka");
-			
+
 			Gdansk.printKursanci();	
 			Gdansk.printKursy();
 
@@ -54,5 +57,30 @@ import java.util.List;
 		}
 
 			Gdansk.printAll();
+			
+			/*---------------------DB-------------------------*/
+			
+				System.out.println(" 000000000000000000  JDBC  ooooooooooooooooooo ");
+				
+	OsrodkiDBManager OsrodkiDB = new OsrodkiDBManager();
+	OsrodkiDB.deleteAllOsrodki();
+	KursDBManager KursDB= new KursDBManager();
+	KursDB.deleteAllKurs();
+	
+	
+	Kurs kurs1 = new Kurs(21, "KursLopata");
+	KursDB.addDBKurs(kurs1);
+	Kurs kurs2 = new Kurs(22 , "KursWidly");
+	KursDB.addDBKurs(kurs2);
+	Kurs kurs3 = new Kurs(23 , "KursPomocy");
+	KursDB.addDBKurs(kurs3);
+
+	
+	Osrodki Osrodki2 = new Osrodki("Sopot", kursanci, kursy);
+	OsrodkiDB.addOsrodki(Osrodki2);
+	Osrodki Osrodki3 = new Osrodki("Gdynia", kursanci, kursy);
+	OsrodkiDB.addOsrodki(Osrodki3);
+
 		}
 	}
+
